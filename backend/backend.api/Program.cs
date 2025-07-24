09fa4430e0307b88a69cbe15266b0b6e06182a36
dean <deanfernandes18@gmail.com>
2025-07-24 15:41:24 +0100

    update backend

--- backend/backend.api/Program.cs
+++ backend/backend.api/Program.cs
@@ -33,6 +33,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/hello", () => "Hello Wrld!");
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
